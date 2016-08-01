aacb137841449958386615321f846009f45144e0
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-01 13:30:35 -0400

    initial commit

---
+++ test/spec.js
@@ -0,0 +1,19 @@
+// describe("the thing i'm testing", function(){
+//   it("should do the right thing", function(){
+//     expect(true).toBe(true);
+//   });
+// });
+
+//
+// var primes = [];
+//
+// for(var i=1; i<=60; i++){
+//   for(var y=2; y<=i; y++){
+//     if(i%y===0){
+//       var not_prime=true;
+//     }
+//     if(not_prime=false){
+//       primes.push(i);
+//     }
+//   };
+// };
